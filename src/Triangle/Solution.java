adfb4b569f042426b087e6a0d0e8a942bc2e03a3
Adrian Pełechaty <adrian.pelechaty95@gmail.com>
2019-04-29 20:29:10 +0200

    Add Triangle

--- src/Triangle/Solution.java
+++ src/Triangle/Solution.java
@@ -5,10 +5,9 @@ import java.util.*;
 public class Solution {
     public int solution(int[] A) {
         Arrays.sort(A);
-
         for(int i = 0; i < A.length-2; i++){
             if(A[i] > 0){
-                if((A[i] + A[i+1] > A[i+2]) || (A[i+1] + A[i+2] > A[i]) || (A[i+2] + A[i] > A[i+1])){
+                if((A[i] + A[i+1] > A[i+2]) && (A[i+1] + A[i+2] > A[i]) && (A[i+2] + A[i] > A[i+1])){
                     return 1;
                 }
             }
